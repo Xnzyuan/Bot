b993cdc4420048a5f5c8cc50886343488722f00e
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2021-12-26 07:00:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -257,7 +257,7 @@ try {
 		const isCmd = body.startsWith(prefix)
 		const arg = budy.slice(command.length + 2, budy.length)
 		const c = args.join(' ')
-                const pop = budy.slice(4)
+                const pop = budy.slice(5)
                 const q = args.join(' ')
 		var pes = (type === 'conversation' && mek.message.conversation) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text ? mek.message.extendedTextMessage.text : ''
 		const messagesD = pes.slice(0).trim().split(/ +/).shift().toLowerCase()
