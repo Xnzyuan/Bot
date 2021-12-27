654d60108752a4bfb598bdd2740b8dfdf9f1330f
Lawliet <94941593+PerwiraKusuma111@users.noreply.github.com>
2021-12-27 08:16:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1064,17 +1064,14 @@ case 'attp':
               break
 case 'menu2':
 conn.relayWAMessage(conn.prepareMessageFromContent(from, {
-                    "buttonMessage": {
 					"groupInviteMessage": {
 						"groupJid": "120363022484633265@g.us",
 						"inviteCode": "5Nib048/CNDLxTgI",
 						"inviteExpiration": "1640826580",
 						"groupName": "Bot WhatsApp",
 						"caption": "\n*List Simple Menu*\n\n*Tools menu*\n .sticker\n .toimg\n .tomp3\n .togif\n .tovideo\n .semoji\n\n*Download Menu*\n .play\n .ytmp3\n .ytmp4\n .igdl\n .tiktok\n .mediafire\n .image\n\n*Maker Menu*\n .photooxy\n .textpro\n .tahta\n .attp\n\n*Fun Menu*\n .dadu\n .suit\n .tictactoe\n .tebakgambar\n .katabijak\n .motivasi\n .pantun\n .bucin\n .fakta\n\n_Awali pesan dengan huruf .bot untuk melakukan chat dengan bot._\n_Contoh: .bot halo_\n\n\n*©Perwira Bot WhatsApp*", 
-jpegThumbnail: fs.readFileSync(`./denz.jpg`), "buttons": 
-[{buttonId:`owner`,buttonText:{displayText:'Owner'},type:1},{buttonId:`oke`,buttonText:{displayText:'Oke'},type:1}]
+jpegThumbnail: fs.readFileSync(`./denz.jpg`)
 					}
-}
 				}, {contextInfo: {}}))
 break
 case 'command':
